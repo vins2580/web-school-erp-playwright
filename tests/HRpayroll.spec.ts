a41fc66649287ca926ffda48fb48c9453e92ad4e
vina <vinayvinsgb@gmail.com>
2026-09-16 13:18:33 +0530

    Add HR Payroll POM and test

--- tests/HRpayroll.spec.ts
+++ tests/HRpayroll.spec.ts
@@ -1,18 +1,15 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
 import { LoginPage } from '../pages/LoginPage';
 import { HRPAYROLLPAGE } from '../pages/HRPayrollPage';
 
-test('Login and click HR/Payroll', async ({ page }) => {
+test('Login and navigate to HR Payroll', async ({ page }) => {
 
     const loginPage = new LoginPage(page);
 
     await loginPage.gotoLoginPage();
 
-    await loginPage.Login(
-        'admin',
-        'webschool'
-    );
+    await loginPage.Login('admin', 'webschool');
 
     const hrPayrollPage = new HRPAYROLLPAGE(page);
 
